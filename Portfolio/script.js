aeba847c30c81a0b90647ee1d46e2d442e0289a3
HarindiNikeshala <113182630+HarindiNikeshala@users.noreply.github.com>
2023-09-20 00:05:42 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,50 @@
+//toggle navigation bar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+            sec.classList.add('show-animate');
+        } else {
+            sec.classList.remove('show-animate')
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxloz_jsIoN4NwXxR0mWKwc1CIcK1QpxlJ2QBx2RCpwkDOfCWI1Qvm9mUQFhEgAk1s2/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
